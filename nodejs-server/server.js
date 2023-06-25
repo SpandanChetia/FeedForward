19be99fe165a5e95d5a387d0e573253cf929dbdb
rajiv <rajivsah240@gmail.com>
2023-06-25 17:57:02 +0530

    server

--- nodejs-server/server.js
+++ nodejs-server/server.js
@@ -11,19 +11,13 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 
-
-
-
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
 // Enable CORS for all routes
 app.use(cors());
-
-
-
-
 
 
 
