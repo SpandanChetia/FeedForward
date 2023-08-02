bd770ed137a0b4104bcca0dfd1bff6bc82d2ec41
SpandanPriyam22 <spandanpriyam1976@gmail.com>
2023-08-02 13:32:35 +0530

    Updated

--- frontend/src/components/Problems.js
+++ frontend/src/components/Problems.js
@@ -9,8 +9,7 @@ import Community from "../assets/community.jpg";
 import Sustainable from "../assets/sustainable.jpg";
 import underline1 from "../assets/underline-heading.png";
 import Arrow1 from "../assets/arrow1.png";
-import Bulb from "../assets/bulb.png";
-import Sun from "../assets/atom.png";
+import Sun from "../assets/Highlight_05.png";
 import Smallstars from "../assets/small-stars.png";
 import Smallstars2 from "../assets/small-stars2.png";
 
@@ -19,8 +18,7 @@ const Problems = () => {
     <div id="problems">
       <div class="the-problems">
         <div class="problems-section1">
-        <h1 id="heading-problems"><img id="small-stars3" src={Smallstars} alt="Sun Photo"/>Why Was FeedForward <span className="created"> Created ? <img id="underline1" src ={underline1} alt = "underline"/></span></h1>
-        <div className="handwritten"><img id="sun" src={Sun} alt="Sun Photo"/><img id="small-stars" src={Smallstars} alt="Sun Photo"/><img id="small-stars1" src={Smallstars2} alt="Sun Photo"/><img id="arrow1" src ={Arrow1} alt = "Down arrow"/></div>
+        <h1 id="heading-problems"><img id="highlight-heading" src={Sun} alt="Sun Photo"/>Why Was FeedForward <span className="created"> Created ? <img id="underline1" src ={underline1} alt = "underline"/></span></h1>
             <div class="cards">
                 <div class="card1">
                     <img id="WasteMoney" src={Wastemoney} alt ="Waste of Money photo"/>
